22d10c93c696630d57bed8b2bf547b2d98c3273c
stevecmd <murimi101@gmail.com>
2024-08-05 01:47:36 +0300

    Revert "Merge pull request #10 from patty6339/SteveCMD"
    This reverts commit 8367ca97b1c68d22f58be5073456ddc07bb622ff, reversing
    changes made to 8b1331d094e5ff1a875aa440a157866942a9fc87.

--- models/city.py
+++ models/city.py
@@ -10,12 +10,14 @@ from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """Representation of city """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
     if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
         places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes city"""
