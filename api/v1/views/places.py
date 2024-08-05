3c83c3f87e639432a8583c38e398dc65a3ac651f
stevecmd <murimi101@gmail.com>
2024-08-05 21:42:31 +0300

    Test 15. Search

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """This module creates a Flask app"""
 
-from flask import jsonify, request, abort
+from api.v1.views import app_views
+from flask import jsonify, abort, make_response, request
+from models.city import City
 from models import storage
 from models.place import Place
-from models.city import City
 from models.user import User
-from api.v1.views import app_views
+import requests
+import json
+from os import getenv
 
 
 @app_views.route(
@@ -85,3 +88,56 @@ def update_place(place_id):
             setattr(place, key, value)
     storage.save()
     return jsonify(place.to_dict()), 200
+
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def places_search():
+    """
+    Search for places based on states, cities, and amenities.
+    """
+    body_r = request.get_json()
+    if body_r is None:
+        abort(400, "Not a JSON")
+
+    if not body_r or (
+            not body_r.get('states') and
+            not body_r.get('cities') and
+            not body_r.get('amenities')
+    ):
+        places = storage.all(Place)
+        return jsonify([place.to_dict() for place in places.values()])
+
+    places = set()
+
+    if 'states' in body_r:
+        for state_id in body_r['states']:
+            state = storage.get("State", state_id)
+            if state:
+                for city in state.cities:
+                    places.update(city.places)
+
+    if 'cities' in body_r:
+        for city_id in body_r['cities']:
+            city = storage.get("City", city_id)
+            if city:
+                places.update(city.places)
+
+    if not places:
+        places = set(storage.all(Place).values())
+
+    if 'amenities' in body_r:
+        amenities = {
+            storage.get("Amenity", amenity_id)
+            for amenity_id in body_r['amenities']
+        }
+        filtered_places = set()
+        for place in places:
+            place_amenities = {
+                storage.get("Amenity", amenity.id)
+                for amenity in place.amenities
+            }
+            if amenities.issubset(place_amenities):
+                filtered_places.add(place)
+        places = filtered_places
+
+    return jsonify([place.to_dict() for place in places])
