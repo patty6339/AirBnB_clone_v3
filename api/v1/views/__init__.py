aacc09d3227a6a19bc91331eaf0af4bb8dcf4ebc
stevecmd <murimi101@gmail.com>
2024-08-05 13:48:45 +0300

    Advanced tasks

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,20 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *
-from api.v1.views.states import *
+"""Import Classes"""
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+"""Import views"""
+from api.v1.views.amenities import *
 from api.v1.views.cities import *
+from api.v1.views.index import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.states import *
+from api.v1.views.users import *
