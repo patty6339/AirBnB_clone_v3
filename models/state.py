b128ff1336e7a8cb98e53d5472bcadcc6c66d417
stevecmd <murimi101@gmail.com>
2024-08-21 23:12:12 +0300

    feat: Add id column to State model
    This commit adds an `id` column to the `State` model in the `state.py` file. The `id` column is defined as a primary key with a length of 60 characters and is not nullable. This change enhances the data model by providing a unique identifier for each state in the database.

--- models/state.py
+++ models/state.py
@@ -13,6 +13,7 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     """Representation of state """
     __tablename__ = 'states'
+    id = Column(String(60), primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
 
     if models.storage_t == "db":
