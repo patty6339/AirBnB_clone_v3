edc2ac72cc953ef6f0533fb3601d121689190e27
stevecmd <murimi101@gmail.com>
2024-08-22 07:58:52 +0300

    feat: Add CORS support to Flask app

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,13 @@
 """This module creates a Flask app"""
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_views)
 
 
